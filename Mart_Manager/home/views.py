1b83fddc3dedef5ffc30f4733250f2ee0117c5d5
Glen furtado <glenfurtado1010@gmail.com>
2024-08-05 11:53:40 +0530

    just part of earlier commit

--- Mart_Manager/home/views.py
+++ Mart_Manager/home/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
+
+def login(request):
+    if request.method == 'POST':
+        name = request.POST["uname"]
+        pasw = request.POST["password"]
+        
+    return render(request, "home/login.html")
