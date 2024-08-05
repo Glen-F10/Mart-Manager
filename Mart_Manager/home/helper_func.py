998acc711c964f14d7e52c0b72bf697c68bfb703
Glen furtado <glenfurtado1010@gmail.com>
2024-08-05 11:52:11 +0530

    just added some code in login page and also a functions file

---
+++ Mart_Manager/home/helper_func.py
@@ -0,0 +1,19 @@
+def checkAuth(uname, pasw):
+    mainFlag = False
+    userDet = [
+            ["Glen", "2001"],
+            ["Gladys", "1965"],
+            ["Lenisha", "2006"],
+            ["Lancy", "1960"]
+        ]
+    if uname or pasw in userDet:
+        for user in userDet:
+            if uname == user[0] and pasw == user[1]:
+                mainFlag = True
+            else:
+                pass
+    else:
+        pass
+    return mainFlag
+
+print(checkAuth("Glen", "2001"))
